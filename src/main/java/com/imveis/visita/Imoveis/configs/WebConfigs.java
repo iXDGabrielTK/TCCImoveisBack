abebe2b50c87309ce8de4dd812235af80437409f
iXDGabrielTK <gabrielferraritanaka@gmail.com>
2024-10-20 18:23:42 -0300

    Feito modificacoes no WebConfigs nao sei se usaremos todas as importações mas eu fiz

--- src/main/java/com/imveis/visita/Imoveis/configs/WebConfigs.java
+++ src/main/java/com/imveis/visita/Imoveis/configs/WebConfigs.java
@@ -1,4 +1,10 @@
 package com.imveis.visita.Imoveis.configs;
 
+import org.springframework.context.annotation.Configuration;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+@Configuration
 public class WebConfigs {
+
 }
--- src/test/java/com/imveis/visita/Imoveis/ImoveisApplicationTests.java
+++ src/test/java/com/imveis/visita/Imoveis/ImoveisApplicationTests.java
@@ -2,12 +2,28 @@ package com.imveis.visita.Imoveis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+@EnableWebMvc
 @SpringBootTest
 class ImoveisApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public Connection getConnection() throws SQLException {
+		var host = System.getenv("DATABASE_HOST");
+		var password = System.getenv("DATABASE_PASSWORD");
+		var user = System.getenv("DATABASE_USER");
+		return DriverManager.getConnection(host, user, password);
+	}
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
 	}
 
 }
