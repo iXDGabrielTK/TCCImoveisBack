4ac8fb9bcb34ff8c2f4395ecf866d4b5bcf18fec
iXDGabrielTK <gabrielferraritanaka@gmail.com>
2024-10-20 21:26:51 -0300

    Adicionado novas dependencias e modificao no WebConfigs, tambem foi mechido nas propriedades da aplicacao, foi corrigido os avisos ao dar commit tambem!

--- src/main/java/com/imveis/visita/Imoveis/configs/WebConfigs.java
+++ src/main/java/com/imveis/visita/Imoveis/configs/WebConfigs.java
@@ -1,5 +1,33 @@
 package com.imveis.visita.Imoveis.configs;
 
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.imveis.visita.Imoveis.repositories")
 public class WebConfigs {
 
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:postgresql://localhost:5432/imoveis_db")
+                .username("postgres")
+                .password("password")
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new JpaTransactionManager();
+    }
 }
+
