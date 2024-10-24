a7ed07f87696d317faf40bed7fa4581d5bbeca48
iXDGabrielTK <gabrielferraritanaka@gmail.com>
2024-10-24 01:33:45 -0300

    Tcc 1.4

--- src/test/java/com/imveis/visita/Imoveis/ImoveisApplicationTests.java
+++ src/test/java/com/imveis/visita/Imoveis/ImoveisApplicationTests.java
@@ -2,28 +2,11 @@ package com.imveis.visita.Imoveis;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-@EnableWebMvc
 @SpringBootTest
 class ImoveisApplicationTests {
 
-	public Connection getConnection() throws SQLException {
-		var host = System.getenv("DATABASE_HOST");
-		var password = System.getenv("DATABASE_PASSWORD");
-		var user = System.getenv("DATABASE_USER");
-		return DriverManager.getConnection(host, user, password);
+	@Test
+	void contextLoads() {
 	}
-
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
-	}
-
 }
