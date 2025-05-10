27ee775c65cd23405724cbf941ffce2ac7aeb328
CaioFabric10 <caiofabricio07@gmail.com>
2025-05-10 03:48:10 -0300

    2.5 - escolher meses para financiamento

--- src/main/java/com/imveis/visita/Imoveis/dtos/SimulacaoRequest.java
+++ src/main/java/com/imveis/visita/Imoveis/dtos/SimulacaoRequest.java
@@ -10,4 +10,5 @@ public class SimulacaoRequest {
 
     private BigDecimal rendaMensal;
     private BigDecimal valorEntrada;
+    private int prazo;
 }
--- src/main/java/com/imveis/visita/Imoveis/service/FinanceService.java
+++ src/main/java/com/imveis/visita/Imoveis/service/FinanceService.java
@@ -6,23 +6,30 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.List;
 
 @Service
 public class FinanceService {
 
     private static final BigDecimal PERCENTUAL_RENDA = new BigDecimal("0.30");
-    private static final int PRAZO_MESES = 360;
+    private static final List<Integer> PRAZO_MESES_PERMITIDOS = List.of(120,180,240,300,360);
+    private int prazo;
 
     public SimulacaoResponse simularFinanciamento(SimulacaoRequest request) {
         BigDecimal rendaMensal = request.getRendaMensal();
         BigDecimal entrada = request.getValorEntrada();
+        int prazo = request.getPrazo();
 
         if(rendaMensal == null || entrada == null || rendaMensal.compareTo(BigDecimal.ZERO) <=  0) {
             throw new IllegalArgumentException("Renda mensal e entrada devem ser válidos");
         }
 
+        if(!PRAZO_MESES_PERMITIDOS.contains(prazo)){
+            throw new IllegalArgumentException("Prazo inválido. Os prazos permitidos são: " + PRAZO_MESES_PERMITIDOS);
+        }
+
         BigDecimal valorMaxParcela = rendaMensal.multiply(PERCENTUAL_RENDA).setScale(2, RoundingMode.HALF_UP);
-        BigDecimal valorFinanciamento = valorMaxParcela.multiply(BigDecimal.valueOf(PRAZO_MESES)).setScale(2,RoundingMode.HALF_UP);
+        BigDecimal valorFinanciamento = valorMaxParcela.multiply(BigDecimal.valueOf(prazo)).setScale(2, RoundingMode.HALF_UP);
         BigDecimal poderDeCompra = entrada.add(valorFinanciamento).setScale(2, RoundingMode.HALF_UP);
 
         return new SimulacaoResponse(valorMaxParcela, valorFinanciamento, poderDeCompra);
